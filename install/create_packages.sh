29125533a994181041c2ded4ca96753e682a7ecd
Kristijan Fremen Velkovski <me@krisfremen.com>
2021-03-24 00:07:15 -0400

    Test

--- install/create_packages.sh
+++ install/create_packages.sh
@@ -8,10 +8,10 @@ PACK_DIR=$(pwd)/linux/release
 
 # Temp dir for creating *.tar.bz2 package
 BUILD_PACK_DIR=$(pwd)/tmp/doublecmd-$(date +%y.%m.%d)
-mkdir -p $BUILD_PACK_DIR
+mkdir -p "$BUILD_PACK_DIR"
 # Create temp dir for building
 BUILD_DC_TMP_DIR=$(pwd)/tmp/doublecmd-$DC_VER
-mkdir -p $BUILD_DC_TMP_DIR
+mkdir -p "$BUILD_DC_TMP_DIR"
 
 help()
 {
